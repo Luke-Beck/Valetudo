1574ad59d16c11d6313e85d408ab4dcca85f250c
LukeAPEX <luke.beck@live.com.au>
2023-10-11 11:40:52 +1000

    Change Oucher volume when setting speaker volume | Untested

--- backend/lib/robots/roborock/capabilities/RoborockSpeakerVolumeControlCapability.js
+++ backend/lib/robots/roborock/capabilities/RoborockSpeakerVolumeControlCapability.js
@@ -1,4 +1,6 @@
 const SpeakerVolumeControlCapability = require("../../../core/capabilities/SpeakerVolumeControlCapability");
+const fs = require("fs");
+const exec = require('child_process');
 
 /**
  * @extends SpeakerVolumeControlCapability<import("../RoborockValetudoRobot")>
@@ -23,6 +25,30 @@ class RoborockSpeakerVolumeControlCapability extends SpeakerVolumeControlCapabil
      */
     async setVolume(value) {
         await this.robot.sendCommand("change_sound_volume", [value], {});
+
+        // If oucher is installed, set the volume there too
+        if (fs.existsSync("/mnt/data/oucher/oucher.yml")) {
+            const oucherConfig = fs.readFileSync("/mnt/data/oucher/oucher.yml", "utf-8");
+            const newOucherConfig = oucherConfig.replace(/volume: \d+/g, `volume: ${value}`);
+
+            fs.writeFileSync("/mnt/data/oucher/oucher.yml", newOucherConfig);
+
+            // Find the oucher process PID if it is running and then restart it
+            try {
+                const pid = exec.execSync("pidof oucher").toString().trim();
+                // if pid is not empty, kill it
+                if (pid.length > 0) {
+                    // if multiple pids are returned, kill them all by splitting them at the spaces and then killing them one by one
+                    pid.split(" ").forEach((pid) => {
+                        exec.execSync(`kill ${pid}`);
+                    });
+                    // now restart oucher
+                    exec.execSync("/mnt/data/oucher/oucher");
+                }
+            } catch (e) {
+                // oucher is not running, nothing to do
+            }
+        }
     }
 }
 
